534c78606a4ba85daea18dcc1b15750c5e22bed1
rishisabharwaldps <75731259+rishisabharwaldps@users.noreply.github.com>
2021-05-15 15:32:00 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+function addUser(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyCbw4reqJeLyYyLyb8pEkrxwkWIFR624k4",
+        authDomain: "project-test-ae6bf.firebaseapp.com",
+        databaseURL: "https://project-test-ae6bf-default-rtdb.firebaseio.com",
+        projectId: "project-test-ae6bf",
+        storageBucket: "project-test-ae6bf.appspot.com",
+        messagingSenderId: "918130248062",
+        appId: "1:918130248062:web:f924464d00ab3afc32cb2f"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+}
